38ca9a5b70a86ab99d409036a5a308fe1c0a693f
Ilya <ilya.tatsenko@yandex.ru>
2021-01-21 01:13:34 +0300

    1 st try

--- include/MyStack.h
+++ include/MyStack.h
@@ -1,9 +1,45 @@
-// Copyright 2020 A.SHT
-#ifndef INCLUDE_MYSTACK_H_
-#define INCLUDE_MYSTACK_H_
+// Copyright 2021 Ilya Tatsenko
+#pragma once
+#include<vector>
 
-template<class T>
+template <class T>
 class MyStack {
-};
+private:
+    std::vector<T>* mas;
+    int length;
 
-#endif  // INCLUDE_MYSTACK_H_
+public:
+    explicit MyStack(int x) : length(x) {
+        mas = new std::vector<T>;
+        mas->reserve(x);
+    }
+    MyStack(const MyStack& x) {
+        mas = x.mas;
+        length = x.length;
+    }
+    ~MyStack() {
+        delete mas;
+        mas = nullptr;
+    }
+    T get() const {
+        return mas->back();
+    }
+    T pop() {
+        T p_ = mas->back();
+        mas->pop_back();
+        return p_;
+    }
+    void push(T x) {
+        if (mas->empty() == 1) mas->insert(mas->begin(), x);
+        else
+            mas->push_back(x);
+    }
+    bool isFull() const {
+        if (mas->size() == length) return true;
+        else
+            return false;
+    }
+    bool isEmpty() const {
+        return mas->empty() == true;
+    }
+};
--- src/postfix.cpp
+++ src/postfix.cpp
@@ -1,7 +1,63 @@
-// Copyright 2020 A.SHT
-#include "MyStack.h"
-#include "postfix.h"
+// Copyright 2021 Ilya Tatsenko
+#include<iostream>
+#include<string>
 
-std::string infix2postfix(std::string infix) {
-  return infix;
+#include"MyStack.h"
+#include"postfix.h"
+
+
+int function(char x) {
+    switch (x) {
+    case '(': return 0; break;
+    case ')': return 1; break;
+    case '+': return 2; break;
+    case '-': return 2; break;
+    case '*': return 3; break;
+    case '/':return 3; break;
+    case ' ':return 100; break;
+    default: return -1;
+    }
+
+}
+string infix2postfix(string x) {
+    MyStack<char> st(x.size());
+    string new_str;
+
+    for (int i = 0; i < x.size(); i++) {
+        if (function(x[i]) == 100) continue;
+        if (function(x[i]) == -1) {
+            if (x[i] == '.' || x[i + 1] == '.') {
+                new_str += x[i];
+                continue;
+            } else {
+                new_str += x[i];
+                new_str += ' ';
+                continue;
+            }
+        }
+        if (function(x[i]) == 1) {
+            while (function(st.get()) != 0) {
+                new_str += st.pop();
+                new_str += ' ';
+            }
+            st.pop();
+            continue;
+        }
+        if (st.isEmpty() || function(x[i]) == 0 || function(x[i]) > function(st.get())) {
+            st.push(x[i]);
+            continue;
+        } else if (function(st.get()) >= function(x[i])) {
+            new_str += st.pop();
+            new_str += ' ';
+            st.push(x[i]);
+            continue;
+        }
+        }
+    if (!st.isEmpty()) {
+        while (!st.isEmpty()) {
+            new_str += st.pop();
+            new_str += ' ';
+        }
+    }
+    return new_str;
 }
